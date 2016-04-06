39fd8f479d3baafbcff09606286668f1ab5c846e
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-06 14:12:47 -0500

    write world class, begin unit test, trouble declaring value at index

---
+++ csharp/GameOfLife/World.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class World
+    {
+        public int[,] spaces{ get; set; }
+
+        public World(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentException();
+            }
+            spaces = new int[size, size];
+        }
+    }
+}
---
+++ csharp/GameOfLifeTests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NotNull()
+        {
+            //arrange
+            World newWorld = new World(8);
+            //assert
+            Assert.IsNotNull(newWorld);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeOrZeroInputThrowsException()
+        {
+            //arrange
+            World newWorld = new World(0);
+        }
+
+        [TestMethod]
+        public void SettingArrayIndices()
+        {
+            //arrange
+            World newWorld = new World(8);
+
+            //act
+            newWorld.spaces[0,0] = ;
+            int[,] expected = new int[,] { {0, 0}, { 0, 0 }, { 2, 2 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+
+            //assert
+            Assert.AreEqual(expected, newWorld);
+        }
+    }
+}
