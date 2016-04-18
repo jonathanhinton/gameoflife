3d54629e6d432c97257d939449147b70e836b27a
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-18 15:58:54 -0500

    multiverse honeycomb

--- csharp/GameOfLife/Multiverse.cs
+++ csharp/GameOfLife/Multiverse.cs
@@ -8,5 +8,29 @@ namespace GameOfLife
 {
     class Multiverse
     {
+        public World block(World currentWorld)
+        {
+            int col = 3;
+            int row = 3;
+            currentWorld.spaces[col, row] = 1;
+            currentWorld.spaces[col, row+1] = 1;
+            currentWorld.spaces[col+1, row] = 1;
+            currentWorld.spaces[col+1, row+1] = 1;
+
+            return currentWorld;
+        }
+
+        public World honeycomb(World currentWorld)
+        {
+            int col = 5;
+            int row = 5;
+            currentWorld.spaces[col, row] = 1;
+            currentWorld.spaces[col-1, row + 1] = 1;
+            currentWorld.spaces[col, row + 3] = 1;
+            currentWorld.spaces[col + 1, row + 2] = 1;
+            currentWorld.spaces[col + 1, row + 1] = 1;
+
+            return currentWorld;
+        }
     }
 }
