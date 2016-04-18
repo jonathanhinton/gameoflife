f543ba573a83ed4b0d899f56bd72fc020e9c4ffb
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-18 16:18:47 -0500

    add toad and random

--- csharp/GameOfLife/Multiverse.cs
+++ csharp/GameOfLife/Multiverse.cs
@@ -10,36 +10,74 @@ namespace GameOfLife
     {
         public World block(World currentWorld)
         {
-            int col = 3;
             int row = 3;
-            currentWorld.spaces[col, row] = 1;
-            currentWorld.spaces[col, row+1] = 1;
-            currentWorld.spaces[col+1, row] = 1;
-            currentWorld.spaces[col+1, row+1] = 1;
+            int col = 3;
+            currentWorld.spaces[row, col] = 1;
+            currentWorld.spaces[row, col+1] = 1;
+            currentWorld.spaces[row+1, col] = 1;
+            currentWorld.spaces[row+1, col+1] = 1;
 
             return currentWorld;
         }
 
         public World honeycomb(World currentWorld)
         {
-            int col = 5;
             int row = 5;
-            currentWorld.spaces[col, row] = 1;
-            currentWorld.spaces[col-1, row + 1] = 1;
-            currentWorld.spaces[col, row + 3] = 1;
-            currentWorld.spaces[col + 1, row + 2] = 1;
-            currentWorld.spaces[col + 1, row + 1] = 1;
+            int col = 5;
+            currentWorld.spaces[row, col] = 1;
+            currentWorld.spaces[row-1, col + 1] = 1;
+            currentWorld.spaces[row, col + 3] = 1;
+            currentWorld.spaces[row + 1, col + 2] = 1;
+            currentWorld.spaces[row + 1, col + 1] = 1;
 
             return currentWorld;
         }
 
         public World threeCellOscilator(World currentWorld)
         {
-            int col = 3;
             int row = 3;
-            currentWorld.spaces[col, row] = 1;
-            currentWorld.spaces[col - 1, row] = 1;
-            currentWorld.spaces[col + 1, row + 3] = 1;
+            int col = 3;
+            currentWorld.spaces[row, col] = 1;
+            currentWorld.spaces[row - 1, col] = 1;
+            currentWorld.spaces[row + 1, col + 3] = 1;
+
+            return currentWorld;
+        }
+
+        public World Toad(World currentWorld)
+        {
+            int row = 5;
+            int col = 5;
+            currentWorld.spaces[row, col] = 1;
+            currentWorld.spaces[row, col+1] = 1;
+            currentWorld.spaces[row, col+2] = 1;
+            currentWorld.spaces[row+1, col-1] = 1;
+            currentWorld.spaces[row+1, col] = 1;
+            currentWorld.spaces[row+1, col+1] = 1;
+
+            return currentWorld;
+
+        }
+
+        public World Random(World currentWorld)
+        {
+            Random random = new System.Random();
+            for (int row = 0; row < currentWorld.spaces.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentWorld.spaces.GetLength(1); col++)
+                {
+                    int randomNumber = (int)Math.Round(random.NextDouble());
+
+                    if (randomNumber == 1)
+                    {
+                        currentWorld.spaces[row, col] = 1;
+                    }
+                    else
+                    {
+                        currentWorld.spaces[row, col] = 0;
+                    }
+                }
+            }
 
             return currentWorld;
         }
