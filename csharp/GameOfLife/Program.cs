0189da2a1a7d5a76befe5ce342cfaeb2e9fe7b44
Jonathan Hinton <hintonjonathan@gmail.com>
2016-04-18 16:07:37 -0500

    setup initial options for program

--- csharp/GameOfLife/Program.cs
+++ csharp/GameOfLife/Program.cs
@@ -10,6 +10,23 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
+            World newWorld = new World(30);
+            Multiverse multiverse = new Multiverse();
+            Rules rules = new Rules();
+
+            Console.WriteLine("Select an option");
+            Console.WriteLine("1 - Block");
+            Console.WriteLine("2 - Honeycomb");
+            Console.WriteLine("3 - Three Cell Oscilator");
+            Console.WriteLine("4 - Toad");
+            Console.WriteLine("5 - Random");
+
+            int x = int.Parse(Console.ReadLine());
+            string keyMessage = "Press a key to start your game";
+
+
+
+
         }
     }
 }
